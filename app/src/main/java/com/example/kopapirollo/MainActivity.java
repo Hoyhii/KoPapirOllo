a3534e3aa7a612afdf16a516e5a3c2dbc532091a
Hoyhii <HoyhiTV@Gmail.com>
2021-10-20 20:41:14 +0200

    Finished Rock,Paper,Scissors in Android Studio.

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.kopapirollo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imgView1, imgView2;
     private Button btn1,btn2,btn3;
-    private TextView textView1,textView2,textView3;
-    private boolean valaszt;
-    private Random rnd;
-    int min = 1;
-    int max = 3;
-    int szamitogep = (int)Math.floor(Math.random()*(max-min+1)+min);
+    private TextView textView3;
+    int tipp,gep,jatekos,szamitogep;
 
 
     @Override
@@ -30,31 +30,28 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(valaszt){
-                    valaszt = false;
-                    kepvalt(1);
-                    szamitogepValt();
-                }
+                tipp=1;
+                kepvalt(tipp);
+                szamitogepValt();
+                winOrLose(szamitogep,tipp);
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(valaszt){
-                    valaszt = false;
-                    kepvalt(2);
-                    szamitogepValt();
-                }
+                tipp=2;
+                kepvalt(tipp);
+                szamitogepValt();
+                winOrLose(szamitogep,tipp);
             }
         });
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(valaszt){
-                    valaszt = false;
-                    kepvalt(3);
-                    szamitogepValt();
-                }
+                tipp=3;
+                kepvalt(tipp);
+                szamitogepValt();
+                winOrLose(szamitogep,tipp);
             }
         });
 
@@ -64,25 +61,92 @@ public class MainActivity extends AppCompatActivity {
     private void kepvalt(int tipp){
         if(tipp == 1){
             imgView1.setImageResource(R.drawable.rock);
-        }if(tipp == 2){
+        }else if(tipp == 2){
             imgView1.setImageResource(R.drawable.paper);
-        }if(tipp == 3){
+        }else if(tipp == 3){
             imgView1.setImageResource(R.drawable.scissors);
         }
     }
     private void szamitogepValt(){
-
+        szamitogep = (int) (Math.random() * 3+1);
         if(szamitogep == 1){
             imgView2.setImageResource(R.drawable.rock);
-        }if(szamitogep == 2){
+        }else if(szamitogep == 2){
             imgView2.setImageResource(R.drawable.paper);
-        }if(szamitogep == 3){
+        }else if (szamitogep == 3){
             imgView2.setImageResource(R.drawable.scissors);
         }
     }
 
-    private void eldontes(){
-        return;
+    @SuppressLint("SetTextI18n")
+    private void winOrLose(int szamitogep, int tipp){
+
+        if (szamitogep == tipp) {
+            Toast.makeText(MainActivity.this, "Döntetlen", Toast.LENGTH_SHORT).show();
+        } else if (szamitogep == 1 && tipp== 2) {
+            Toast.makeText(MainActivity.this, "VICTORY", Toast.LENGTH_SHORT).show();
+            jatekos++;
+            textView3.setText("Eredmény: Ember: " + jatekos + " Computer: " + gep);
+        } else if (szamitogep == 1 && tipp== 3) {
+            Toast.makeText(MainActivity.this, "DEFEAT", Toast.LENGTH_SHORT).show();
+            gep++;
+            textView3.setText("Eredmény: Ember: " + jatekos + " Computer: " + gep);
+        } else if (szamitogep == 2 && tipp== 1) {
+            Toast.makeText(MainActivity.this, "DEFEAT", Toast.LENGTH_SHORT).show();
+            gep++;
+            textView3.setText("Eredmény: Ember: " + jatekos + " Computer: " + gep);
+        } else if (szamitogep == 2 && tipp== 3) {
+            Toast.makeText(MainActivity.this, "VICTORY", Toast.LENGTH_SHORT).show();
+            jatekos++;
+            textView3.setText("Eredmény: Ember: " + jatekos + " Computer: " + gep);
+        } else if (szamitogep == 3 && tipp== 1) {
+            Toast.makeText(MainActivity.this, "VICTORY", Toast.LENGTH_SHORT).show();
+            jatekos++;
+            textView3.setText("Eredmény: Ember: " + jatekos + " Computer: " + gep);
+        } else if (szamitogep == 3 && tipp== 2) {
+            Toast.makeText(MainActivity.this, "DEFEAT", Toast.LENGTH_SHORT).show();
+            gep++;
+            textView3.setText("Eredmény: Ember: " + jatekos + " Computer: " + gep);
+        }
+
+        if (gep == 3 || jatekos == 3){
+            vege();
+        }
+
+    }
+    public void vege(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Szeretné csapatni egy új gameszkót?");
+        if (gep > jatekos){
+            builder.setTitle("Vereség");
+        } else{
+            builder.setTitle("Győzelem");
+        }
+        builder.setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                uj();
+            }
+        });
+        builder.setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+
+        builder.create().show();
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void uj(){
+        gep = 0;
+        jatekos = 0;
+        textView3.setText("Eredmény: Ember: 0 Computer: 0");
+        imgView1.setImageResource(R.drawable.rock);;
+        imgView2.setImageResource(R.drawable.rock);;
     }
 
     private void init(){
@@ -91,8 +155,6 @@ public class MainActivity extends AppCompatActivity {
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
-        textView1 = findViewById(R.id.textView1);
-        textView2 = findViewById(R.id.textView2);
         textView3 = findViewById(R.id.textView3);
     }
 }
